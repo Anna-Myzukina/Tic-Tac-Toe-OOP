eaacdb46bb68e1fc20fdaf58c69133a366ff1440
Anna Myzukina <anna.muzykina83@gmail.com>
2019-09-17 20:08:52 +0300

    45line

--- bin/main.rb
+++ bin/main.rb
@@ -42,7 +42,8 @@ def turn_count
 end
 
 def current_player
-  return (turn_count % 2).zero? ? player = 'X' : player = 'O'
+  (turn_count % 2).zero? ? player = 'X' : player = 'O'
+  player
 end
 
 def move(board, index, current_player)
