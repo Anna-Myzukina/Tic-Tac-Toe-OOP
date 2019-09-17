d7b907d6eae27a096ec719d8b3ece380d28778e9
macnick <nick@macexperts.gr>
2019-09-17 19:45:13 +0300

    L45 error again

--- bin/main.rb
+++ bin/main.rb
@@ -42,7 +42,7 @@ def turn_count
 end
 
 def current_player
-  (turn_count % 2).zero? ? player = 'X' : player = 'O'
+  return (turn_count % 2).zero? ? player = 'X' : player = 'O'
 end
 
 def move(board, index, current_player)
