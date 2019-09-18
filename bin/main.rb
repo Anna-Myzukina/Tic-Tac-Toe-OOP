add6ceef26f616843c7662645ad3a3b6e7fa8440
macnick <nick@macexperts.gr>
2019-09-18 14:26:38 +0300

    Change methods to show how the interface will work

--- bin/main.rb
+++ bin/main.rb
@@ -2,56 +2,71 @@
 
 require_relative '../lib/player'
 
-def display_board(the_board)
-  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
-  puts '-----------'
-  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
-  puts '-----------'
-  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
+
+class Tictactoe
+  attr_accessor :board, :name
+
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @name = []
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '-----------'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '-----------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " "
+  end
+  
+  def move_board (idx) 
+    @board[idx] = 'X'
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def turn
+    puts 'Choose a spot between 1-9'
+    spot = gets.strip
+    spot = input_to_index(spot)
+    display_board
+  end
+  
+  def turn_count
+    taken = 0
+    @board.each do |i|
+      taken += 1 unless i.nil?
+    end
+    taken
+  end
+  
+  def current_player
+    turn_count.even? ? player = 'X' : player = 'O'
+    return player
+  end
+  
+  def user_name
+    2.times do |i|
+      puts "Please enter name of player #{i + 1}:"
+      @name << gets.strip
+    end
+  end
+
 end
+
+
 puts 'Welcome to Tic Tac Toe'
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-display_board(board)
+puts " "
 
-def user_name
-  name = []
-  2.times do |i|
-    puts "Please enter name of player #{i + 1}:"
-    name << gets.strip
-  end
+board = Tictactoe.new
+
+board.display_board
+board.user_name
+3.times do |i|
+  board.turn
+  board.move_board(i)
+  board.display_board
 end
-
-def turn
-  puts 'Choose a spot between 1-9'
-  spot = gets.strip
-  spot = input_to_index(spot)
-  if valid_move?(spot)
-    move(spot, current_player)
-  else
-    turn
-  end
-  display_board
-end
-
-def turn_count
-  taken = 0
-  @board.each do |i|
-    taken += 1 unless i.nil?
-  end
-  taken
-end
-
-def current_player
-  (turn_count % 2).zero? ? player = 'X' : player = 'O'
-  return player
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-  puts "Now, #{current_player} move is displayed on the board"
-end
-
-current_player
-turn
-
-display_board(board)
--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+module Player
+  def self.move(board, index, current_player)
+    board[index] = current_player
+    p board
+    puts "Now, #{current_player} move is displayed on the board"
+  end
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == ' ')
-end
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == ' ')
+  end
 
-def valid_move?(board, index)
-  index.between?(0, 8) && !position_taken?(board, index)
+  def valid_move?(board, index)
+    index.between?(0, 8) && !position_taken?(board, index)
+  end
 end
