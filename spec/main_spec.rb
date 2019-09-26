8fd91303ffd56ba3fb684a90433af60ce686457c
Anna Myzukina <anna.muzykina83@gmail.com>
2019-09-26 17:00:06 +0300

    Create main_spec file

---
+++ spec/main_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require '../bin/main'
+RSpec.describe Tictactoe do
+  describe '#move_board' do
+    it 'takes position on board of current player and return current player symbol on board' do
+      idx = 2
+      boardX= [' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+      boardO= [' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ']
+      expect(game.move_board(idx)).to eq(boardX)
+      expect(game.move_board(idx)).to eq(boardO)
+    end
+  end
+end
