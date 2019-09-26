dfa143835b55851718428acfbad390ceb1b20f5b
macnick <nick@macexperts.gr>
2019-09-26 14:05:59 +0300

    Add test for position_taken?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,28 @@
+# frozen_string_literal: true
+
 require '../lib/game'
 
 RSpec.describe Game do
-describe '#valid_move?' do
-  it 'returns true/false based on whether the position is already occupied' do
-    game = Game.new
-    board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-        index = 0
-        expect(game.valid_move?(board, index)).to be_truthy
+  describe '#valid_move?' do
+    it 'returns true/false based on whether the position is already occupied' do
+      game = Game.new
+      board = [' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
+      index = 0
+      expect(game.valid_move?(board, index)).to be_truthy
 
-        index = 4
-        expect(game.valid_move?(board, index)).to be_falsey
+      index = 4
+      expect(game.valid_move?(board, index)).to be_falsey
+    end
   end
-end
-describe '#position_taken?' do
-  it 'returns true/false'
-end
+  describe '#position_taken?' do
+    it 'returns true/false based on whether the position is taken' do
+      game = Game.new
+      board = [' ', ' ', 'O', ' ', 'X', ' ', ' ', ' ', ' ']
+      index = 2
+      expect(game.position_taken?(board, index)).to be_truthy
+
+      index = 5
+      expect(game.position_taken?(board, index)).to be_falsey
+    end
+  end
 end
