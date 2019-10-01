e0090ef1b7a56d0cbbe1b2b7e391acab97f7a6d0
macnick <nick@macexperts.gr>
2019-10-01 12:47:15 +0300

    Remove board.rb

--- lib/board.rb
+++
@@ -1,9 +0,0 @@
-class Board
-  def display_board
-    " #{@board[0]} | #{@board[1]} | #{@board[2]}\n
-    -----------\n
-    #{@board[3]} | #{@board[4]} | #{@board[5]}\n
-    -----------
-    #{@board[6]} | #{@board[7]} | #{@board[8]}\n\n"
-  end
-end
