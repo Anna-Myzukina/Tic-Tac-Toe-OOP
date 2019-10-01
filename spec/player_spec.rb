8b151064b9ba81af70b6cf0cfb599a9bb8b93493
Anna Myzukina <anna.muzykina83@gmail.com>
2019-10-01 12:43:07 +0300

    test for Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,8 +2,13 @@
 
 require_relative '../lib/player'
 
-describe ' ' do
-  it '' do
-    expect().to 
+describe '#initialize' do
+  it 'initialize name for player' do
+    player = Player.new('Anna', 'X')
+    expect(player.instance_variable_get(:@name)).to eq('Anna')
+  end
+  it 'initialize symbol for player' do
+    player = Player.new('Anna', 'X')
+    expect(player.instance_variable_get(:@symb)).to eq('X')
   end
 end
